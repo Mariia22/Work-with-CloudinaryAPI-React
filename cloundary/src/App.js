55f2285278afb7624516ff3e1763d30615458b9b
Maria22 <m.ushakova22@gmail.com>
2021-11-16 17:30:37 +0100

    Download data from localhost

--- cloundary/src/App.js
+++ cloundary/src/App.js
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import images from './api-mock.json'
+import { getImages } from './api';
 
 function App() {
-  const [imageList, setImageList] = useState(images.resources);
+  const [imageList, setImageList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseJson = await getImages();
+      setImageList(responseJson.resources);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="container">
       {imageList.map(image => <img src={image.url} alt={image.public_id} />)}
